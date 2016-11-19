a623cb0469a73d7b18b098be57e48428e02a200f
Divay Prakash <divayprakash3@gmail.com>
2016-11-19 23:13:49 +0530

    Update index.js to use Jade as templating engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
   res.send('<html><body><h1>Hello World</h1></body></html>');
 });
