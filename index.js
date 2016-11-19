93887b603f388b1e91ea5b9fd41d4ccea518d942
Divay Prakash <divayprakash3@gmail.com>
2016-11-19 23:10:28 +0530

    Return 404 on invalid routes

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.send('<html><body><h1>Hello World</h1></body></html>');
 });
 
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
