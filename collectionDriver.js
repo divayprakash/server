f3e6fc9a336ca5215265512e7fa51dd6164c4602
Divay Prakash <divayprakash3@gmail.com>
2016-11-20 00:28:01 +0530

    Update collectionDriver.js to support update and delete

--- collectionDriver.js
+++ collectionDriver.js
@@ -49,4 +49,30 @@ CollectionDriver.prototype.save = function(collectionName, obj, callback) {
     });
 };
 
+CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback(error);
+        else {
+            obj._id = ObjectID(entityId);
+            obj.updated_at = new Date();
+            the_collection.save(obj, function(error,doc) {
+                if (error) callback(error);
+                else callback(null, obj);
+            });
+        }
+    });
+};
+
+CollectionDriver.prototype.delete = function(collectionName, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+        if (error) callback(error);
+        else {
+            the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { //B
+                if (error) callback(error);
+                else callback(null, doc);
+            });
+        }
+    });
+};
+
 exports.CollectionDriver = CollectionDriver;
