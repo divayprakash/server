d2f5a35dc81cdd263ad75ff3fee59c26e986a5dd
Divay Prakash <divayprakash3@gmail.com>
2016-11-20 00:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require('http'),
     path = require('path'),
     MongoClient = require('mongodb').MongoClient,
 	Server = require('mongodb').Server,
-	CollectionDriver = require('./collectionDriver').CollectionDriver;
+	CollectionDriver = require('./collectionDriver').CollectionDriver,
+	bodyParser = require('body-parser');
  
 var app = express();
 app.set('port', process.env.PORT || 3000); 
@@ -22,6 +23,10 @@ MongoClient.connect(url, function(error, db) {
 	collectionDriver = new CollectionDriver(db);
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/:collection', function(req, res) {
@@ -51,6 +56,15 @@ app.get('/:collection/:entity', function(req, res) {
    } else {
       res.send(400, {error: 'bad url', url: req.url});
    }
+});
+
+app.post('/:collection', function(req, res) {
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); } 
+          else { res.send(201, docs); }
+     });
 });
 
 app.use(function (req,res) {
